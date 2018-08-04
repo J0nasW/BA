fa97d32ce7df0796845ce5a1a94c5ad9e00f579c
Jonas W <31929262+J0nasW@users.noreply.github.com>
2018-08-04 09:26:47 +0200

    New Package hyperopt

---
+++ modules/parameter_opt.py
@@ -0,0 +1,20 @@
+"""
+PARAMETER OPTIMIZATION with Hyperopt
+
+IMPORT BY:  <parameter_opt.py>
+
+RETURN:     Several Parameters
+
+INFO:       All Parameters can be changed only here and spread over the whole Project
+            Motor Neurons: FWD, REV
+            Sensory Neurons: PVD, PLM, AVM, ALM
+            Inter Neurons: AVA, AVD, PVC, AVB
+"""
+
+from hyperopt import fmin, tpe, hp
+
+best = fmin(fn=lambda x: x ** 2,
+    space=hp.uniform('x', -10, 10),
+    algo=tpe.suggest,
+    max_evals=100)
+print (best)
